24a349bbaf5ccf029facf59b779c0c1c235144e2
Rene Mondragon <rene.mondragonr@gmail.com>
2020-07-04 11:19:28 -0700

    test

---
+++ Ex1.py
@@ -0,0 +1,11 @@
+#for x in range(1,11):
+#    print(x)
+
+#words=['equites', 'fixed income', 'derivatives', 'fx']  
+#for word in words:
+#    print(word)
+
+x = 'Yes'
+while x == 'Yes':
+    print('Lets go')
+    x = input('Do you want to continue the loop?')
